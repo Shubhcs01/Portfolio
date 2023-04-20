2155818bd379bf9566f3d3d3c5227b39632b5362
Shubham Gupta <shubhcs01@gmail.com>
2023-04-20 15:38:27 +0530

    about page added

--- src/components/Aboutpage/Aboutpage.js
+++ src/components/Aboutpage/Aboutpage.js
@@ -9,12 +9,12 @@ function Aboutpage() {
             <Container>
                 <Row className='textbackground'>
                     <Col md={7} >
-                        <h3 className='aboutmetext'>About <span>Me</span></h3>
-                        <p className='aboutdetails'>I'm a self-taught Front End developer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
+                        <h3 className='aboutmetext' >About <span>Me</span></h3>
+                        <p className='aboutdetails'>I'm a self-taught Web Developer from India. I build websites with a focus on providing the experience for everyone using them and responsiveness. Curious to learn more about developing scalable distribution systems, love problem solving, and care about writing and maintainable code. A responsive design makes your website accessible to all users, regardless of their devices.
                         </p>
                         <ul className='skilllist'>
                             <Row>
-                                <h3>Skills</h3>
+                                <h3 style={{color:"#feec00"}}>Skills</h3>
                                 <Col md={7}>
                                     <li>HTML/CSS</li>
                                     <li>JavaScript</li>
