abfed471630c2d49342449a8e393fdcecab87140
Shubham Gupta <shubhcs01@gmail.com>
2023-04-21 20:25:48 +0530

    blog-section-updated

--- src/components/BlogPage/BlogPage.js
+++ src/components/BlogPage/BlogPage.js
@@ -13,9 +13,9 @@ function BlogPage() {
                     <div className='blogCard'>
                         <img className='blogImg' src={gfg}></img>
                         <h2>Difference Between poll() and remove() method of Queue Interface in Java</h2>
-                        <Button className="blogBtn" href="https://www.geeksforgeeks.org/difference-between-poll-and-remove-method-of-queue-interface-in-java/" target="_blank">
-                            Read Article
-                        </Button>
+                        <button className="blogBtn" href="https://www.geeksforgeeks.org/difference-between-poll-and-remove-method-of-queue-interface-in-java/" target="_blank">
+                            Read
+                        </button>
                     </div>
                 </Row>
 
@@ -23,9 +23,9 @@ function BlogPage() {
                     <div className='blogCard'>
                         <img className='blogImg' src={medium}></img>
                         <h2>Basic concepts and tasks to get started with Linux</h2>
-                        <Button className="blogBtn" href="https://medium.com/@shubh4amg/basic-concepts-and-tasks-to-get-started-with-linux-b6ef0b713616" target="_blank">
-                            Read Article
-                        </Button>
+                        <button className="blogBtn" href="https://medium.com/@shubh4amg/basic-concepts-and-tasks-to-get-started-with-linux-b6ef0b713616" target="_blank">
+                            Read
+                        </button>
                     </div>
                 </Row>
 
@@ -33,9 +33,9 @@ function BlogPage() {
                     <div className='blogCard'>
                         <img className='blogImg' src={medium}></img>
                         <h2>Step-by-step Guide on How To Start an Open Source Contribution</h2>
-                        <Button className="blogBtn" href="https://medium.com/@shubh4amg/step-by-step-guide-on-how-to-start-an-open-source-contribution-db7f1a47f917" target="_blank">
-                            Read Article
-                        </Button>
+                        <button className="blogBtn" href="https://medium.com/@shubh4amg/step-by-step-guide-on-how-to-start-an-open-source-contribution-db7f1a47f917" target="_blank">
+                            Read
+                        </button>
                     </div>
                 </Row>
                
